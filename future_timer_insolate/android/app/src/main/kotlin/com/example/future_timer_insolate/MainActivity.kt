07f0de730fc3b52087b1048837a6204ba455036f
Cristhian-Cruz <cristhian.cruz01@uceva.edu.co>
2025-04-03 15:14:34 -0500

    Remove unnecessary files

--- future_timer_insolate/android/app/src/main/kotlin/com/example/future_timer_insolate/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.future_timer_insolate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- future_timer_insolate/lib/router.dart
+++
@@ -1,6 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'screens/home.dart';
-
-final GoRouter router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => Home())],
-);
